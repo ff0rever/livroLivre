e16ac73d82e777b58d1213079223cffe71e7e895
lauramrqs <lauramrqs@users.noreply.github.com>
2022-03-24 20:44:58 -0300

    Commit

---
+++ atp /inicio.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+ <html> 
+    <head> 
+
+        <title> livrolivro </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <link href="css/estilo.css" rel="stylesheet">
+    </head> 
+
+    <body>
+        <header>
+            <h2> hello, reader!</h2><br/>
+        </header>
+     
+        <section>
+            <nav>
+              <ul>
+                <li><a href="inicio.php">Inicio</a></li>
+                <li><a href="listalivros.php">Livros disponivéis :)</a></li>
+                <li><a href="cadlivros.php">Espreste seu livro :)</a></li>
+                <li><a href="cadusuarios.php">Se Cadastre! </a></li>    
+                <li><a href="index.php">Login</a></li>    
+              </ul>
+            </nav>
+            
+            <article>
+              <h1>livro.livre | empreste livros e tenha uma biblioteca enorme em seu coração </h1></br>
+
+              <p> A livrolivre foi criada com o intuito de facilitar a vida dos leitores e amantes de livros físicos, que adoram ler
+                e poder fazer suas anotações e até dobrar a ponta dos livros (não que a gente faça isso, ok?).  </p>
+
+              <p>E como funciona? Bom, é bem simples! 
+                Você confere a lista dos livros disponíveis no momento na nossa lista de <i> livros disponíveis</i>, 
+                e entra em contato com o dono(a) do livro que você gostaria de emprestar.  </p>
+              
+              <p>Fundada em 2020 em meio ao caos, a l.l já está presente em todas as regiōes do país. </p>
+
+              <img src="https://i.pinimg.com/564x/f1/29/93/f1299356706bd81065d4073774cc78de.jpg" width="800" height="130">
+            </article>
+          </section> 
+
+    </body>  
+
+
+    <footer>
+         <p>boa leitura, volte sempre!</p>
+    </footer>
+   
+ </html>
+
+
+ 
+ 
